e933e766b21f558e8d4fddb01bfe8f67bb77c180
Barseana Simond <barseana@gmail.com>
2019-08-29 05:40:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 function shoppingList() {
   $("#js-shopping-list-form").submit(e => { 
-    
-    e.preventDefault();
+      e.preventDefault();
 
     // capture value that is in input field
-    $("#shopping-list-entry").click(function(){
-      $("input").val()
+  $("#shopping-list-entry").submit(e => {
+      e.preventDefault();
+     const item = $('.shopping-list-entry').val();
     });
     // insert that value into the <li> element that you can find in the html page that is currently creating the list items
-    $('.shopping-list').add("val")
+    $('li').val()
     // append that string to the <ul> item
-    $("ul").append ("<li></li>") 
+    $(".shopping-list").append(
+    `<li>
+    <span class="shopping-item">${item}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`); 
     //clear the input field
     $('.js-shopping-list-entry').val('');
   })
